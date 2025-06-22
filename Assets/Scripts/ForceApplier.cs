347eed8f01c3e9b33273f8d7eae63c7e17b45a40
Quochuy61 <huytnthht@gmail.com>
2025-06-22 13:50:49 +0700

    Update BodyPhysics

---
+++ Assets/Scripts/ForceApplier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ForceApplier : MonoBehaviour
+{
+    private BodyPhysics body;
+
+    private void Start()
+    {
+        body = GetComponent<BodyPhysics>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Áp lực hướng lên khi nhấn Space
+            body.AddForce(new Vector3(0, 200f, 0));
+        }
+    }
+}
